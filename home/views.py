7d5f66e4efc380b8d831bba2dcd09981f99ca371
siesto1elemento <rohitojha9720@gmail.com>
2024-04-05 20:24:33 +0530

    Added expense models to admin site

--- home/views.py
+++ home/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import *
+
 
 def home(request):
-    return render(request, 'home.html')
+    profile = Profile.objects.filter(user= request.user).first()
+    context = {
+        'profile': profile
+    }
+    return render(request, 'home.html',context)
